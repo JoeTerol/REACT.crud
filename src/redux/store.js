e0d34fd030ead5ef1ad19cceb0e52835d3001d5f
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-09 19:51:25 -0300

    taskslice created

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from "../redux/features/taskSlice"
 
 export const store = configureStore({
-    reducer : {
+       task: taskReducer
 
     }
-})
+)
